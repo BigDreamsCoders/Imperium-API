e33266fa0b232846eda28731b16a373134986e99
petrlr14 <pedrodeveloper14@gmail.com>
2020-10-01 15:41:12 -0600

    add accesso control configuration

---
+++ src/config.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from '@hapi/joi';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        DB_HOST: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+        DB_USERNAME: Joi.string().required(),
+        DB_PASSWORD: Joi.string().required(),
+        DB_DATABASE: Joi.string().required(),
+        JWT_SECRET: Joi.string().required(),
+        JWT_EXPIRE: Joi.string().required(),
+      }),
+    }),
+  ],
+})
+export class ConfModule {}
